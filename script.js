4a8547f23a9356b30e3c2f8b1fb895114ee2726d
Abusyead <abusyead1000@gmail.com>
2025-02-21 13:13:10 +0800

    output js text

--- script.js
+++ script.js
@@ -3,9 +3,19 @@ const addBtn = document.getElementById('addBtn');
 const output = document.getElementById('output');
 
 addBtn.addEventListener('click' , () =>{
-    const inputText = inputBox.value.trim();
+    const inputText = textInput.value.trim();
     if(inputText.length  <= 0){
         alert('enter your value')
         return false;
     }
+
+    const li = document.createElement('li')
+    li.classList.add("list-none" ,"bg-white" ,"mt-3" , "p-2","rounded")
+
+    const p = document.createElement('p')
+    p.innerHTML = inputText;
+    li.appendChild(p);
+
+    output.appendChild(li);
+    textInput.value = ""
 })
